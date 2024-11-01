b7fe5df5cea9bf64b55ee3fd838307a7ae30e5b2
yzaremskaya <yzaremskaya@ptsecurity.com>
2024-11-01 17:10:36 +0300

    4-2-11 (finished)

--- TODO_task_040211_log_generator.py
+++ TODO_task_040211_log_generator.py
@@ -1,24 +1,13 @@
 #!/bin/python
 
-# работает через echo
-
-# поток ввода - Список IP в формате: ['XXX.XXX.XXX.XXX', 'XXX.XXX.XXX.XXX']
-# поток ввода - список команд, которые нужно выполнить на хостах в формате: ['command', 'command']
-# сформировать лог-сообщения, какие команды были выполнены на хосте: На хосте 123.123.123.123 была выполнена команда ls -la
-# Если была встречена команда 'rm -rf /' - то нужно остановить выполнение всей программы
-# и вывести сообщение: На хосте 321.321.321.321 была попытка выполнить команду rm -rf / , но процесс выполнения был аварийно остановлен.
-# Значения будут подавать автоматически следующим способом: echo -e "['123.123.123.123', '321.321.321.321']\n['ls -la', 'ps uxwww']" | python3 your_programm.py
-
-import sys, os, ast
+import sys, os, ast, itertools
 
 ip_addresses_and_commands = input_data = sys.stdin.read()
-ips, cmds = ip_addresses_and_commands.split('\\n')
+#ips, cmds = ip_addresses_and_commands.split('\n')
+ips, cmds = ip_addresses_and_commands.splitlines()
 
 ip_addresses = ast.literal_eval(ips)
 commands = ast.literal_eval(cmds)
-
-# ip_addresses = ips.translate({ord(i): None for i in '[]"\''}).split(', ')
-# commands = cmds.translate({ord(i): None for i in '[]"\''}).split(', ')
 
 
 def generate_hosts(my_ip):
@@ -30,7 +19,8 @@ def generate_commands(my_cmd):
         yield x
 
 def main():
-    for k, v in zip(generate_hosts(ip_addresses), generate_commands(commands)):
+    for k, v in itertools.product(generate_hosts(ip_addresses), generate_commands(commands)):
+            #zip(generate_hosts(ip_addresses), generate_commands(commands))):
         if v != 'rm -rf /':
             print('На хосте', k, 'была выполнена команда', v)
         else:
@@ -40,3 +30,18 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+# работает через echo
+
+# поток ввода - Список IP в формате: ['XXX.XXX.XXX.XXX', 'XXX.XXX.XXX.XXX']
+# поток ввода - список команд, которые нужно выполнить на хостах в формате: ['command', 'command']
+# сформировать лог-сообщения, какие команды были выполнены на хосте: На хосте 123.123.123.123 была выполнена команда ls -la
+# Если была встречена команда 'rm -rf /' - то нужно остановить выполнение всей программы
+# и вывести сообщение: На хосте 321.321.321.321 была попытка выполнить команду rm -rf / , но процесс выполнения был аварийно остановлен.
+# Значения будут подавать автоматически следующим способом: echo -e "['123.123.123.123', '321.321.321.321']\n['ls -la', 'ps uxwww']" | python3 your_programm.py
+
+
+# ip_addresses = ips.translate({ord(i): None for i in '[]"\''}).split(', ')
+# commands = cmds.translate({ord(i): None for i in '[]"\''}).split(', ')
